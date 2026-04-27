c8b78d027b41bbe807362151d3c1b6e61522fe3b
yaokang <oyaknga@gmail.com>
2026-04-27 10:22:14 +0800

    m

--- genCommit.bat
+++ genCommit.bat
@@ -1,7 +1,7 @@
 set curDir="%~dp0%"
 cd /d %curDir%
 
-python gen_resources.py --input %curDir%/_posts
+python gen_resources.py --input "%curDir%_posts"
 git add ./
 git commit -m "m"
 git push -u origin main
